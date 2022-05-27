577e49c04ace8f19cc7cf21b1c9bc5a865e6491a
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-05-27 22:48:19 +0900

    Add temp frontend for dev use

---
+++ temp-web/src/components/ItemList/ItemList.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+
+interface Item {
+  id: number;
+  name: string;
+  category: string;
+  image_filename: string;
+};
+
+const server = process.env.API_URL || 'http://127.0.0.1:9000';
+const placeholderImage = process.env.PUBLIC_URL + '/logo192.png';
+
+interface Prop {
+  reload?: boolean;
+  onLoadCompleted?: () => void;
+}
+
+export const ItemList: React.FC<Prop> = (props) => {
+  const { reload = true, onLoadCompleted } = props;
+  const [items, setItems] = useState<Item[]>([])
+  const fetchItems = () => {
+    fetch(server.concat('/items'),
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('GET success:', data);
+        setItems(data.items);
+        onLoadCompleted && onLoadCompleted();
+      })
+      .catch(error => {
+        console.error('GET error:', error)
+      })
+  }
+
+  useEffect(() => {
+    if (reload) {
+      fetchItems();
+    }
+  }, [reload]);
+
+  return (
+    <div className='ItemListGrid'>
+      { items.map((item) => {
+        return (
+          <div key={item.id} className='ItemList'>
+            {/* TODO: Task 1: Replace the placeholder image with the item image */}
+            <div className="ItemImage" style={{ 
+              backgroundImage: `url(${server}/image/${item.image_filename})`
+              }}>
+            </div>
+            <div className="ItemDescriptions">
+              <span className="ItemName">{item.name}</span>
+              <span className="ItemCategory">{item.category}</span>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+};
---
+++ temp-web/src/components/Listing/Listing.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+const server = process.env.API_URL || 'http://127.0.0.1:9000';
+
+interface Prop {
+  onListingCompleted?: () => void;
+}
+
+type formDataType = {
+  name: string,
+  category: string,
+  image: string | File,
+}
+
+export const Listing: React.FC<Prop> = (props) => {
+  const { onListingCompleted } = props;
+  const initialState = {
+    name: "",
+    category: "",
+    image: "",
+  };
+  const [values, setValues] = useState<formDataType>(initialState);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.files![0] });
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData()
+    data.append('name', values.name)
+    data.append('category', values.category)
+    data.append('image', values.image)
+
+    fetch(server.concat('/items'), {
+      method: 'POST',
+      mode: 'cors',
+      body: data,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('POST success:', data);
+        onListingCompleted && onListingCompleted();
+        setValues(initialState);
+        (document.getElementById('image') as HTMLInputElement).value = "";
+      })
+      .catch((error) => {
+        console.error('POST error:', error);
+      })
+  };
+  return (
+    <div className='Listing'>
+      <form autoComplete='off' onSubmit={onSubmit}>
+        <div className='InputBox'>
+          <input type='text' name='name' id='name' placeholder='name' maxLength={30} value={values.name} onChange={onChange} required />
+          <input type='text' name='category' id='category' placeholder='category' maxLength={12} value={values.category} onChange={onChange} required/>
+          <input type='file' name='image' id='image' accept='.jpg' onChange={onImageChange} required />
+        </div>
+        <div className='SubmitButton'>
+          <button type='submit'>List this item</button>
+        </div>
+      </form>
+    </div>
+  );
+}
