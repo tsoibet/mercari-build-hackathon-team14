b87e40e9b2396e1e929a1c3b0935a7034a24e1f6
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2022-05-31 01:09:17 +0800

    edit homepage styles

--- frontend/src/components/ItemList/ItemList.tsx
+++ frontend/src/components/ItemList/ItemList.tsx
@@ -1,84 +1,93 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 
 interface Item {
-  id: number;
-  name: string;
-  category: string;
-  image_filename: string;
-  isImage: boolean;
-};
+	id: number;
+	name: string;
+	category: string;
+	image_filename: string;
+	isImage: boolean;
+}
 
-const server = process.env.API_URL || 'http://127.0.0.1:9000';
+const server = process.env.API_URL || "http://127.0.0.1:9000";
 
 interface Prop {
-  reload?: boolean;
-  onLoadCompleted?: () => void;
+	reload?: boolean;
+	onLoadCompleted?: () => void;
 }
 
 export const ItemList: React.FC<Prop> = (props) => {
-  const { reload = true, onLoadCompleted } = props;
-  const [items, setItems] = useState<Item[]>([])
-  const fetchItems = useCallback(() => {
-    fetch(server.concat('/items'),
-      {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('GET success:', data);
-        for (var i = 0; i < data.items.length; i++) {
-          if (data.items[i].image_filename.includes(".jpg"))
-            data.items[i].isImage = true
-          else
-            data.items[i].isImage = false
-        }
-        setItems(data.items);
-        console.log('GET success:', data.items);
-        onLoadCompleted && onLoadCompleted();
-      })
-      .catch(error => {
-        console.error('GET error:', error)
-      })
-  }, [onLoadCompleted])
+	const { reload = true, onLoadCompleted } = props;
+	const [items, setItems] = useState<Item[]>([]);
+	const fetchItems = useCallback(() => {
+		fetch(server.concat("/items"), {
+			method: "GET",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("GET success:", data);
+				for (var i = 0; i < data.items.length; i++) {
+					if (data.items[i].image_filename.includes(".jpg"))
+						data.items[i].isImage = true;
+					else data.items[i].isImage = false;
+				}
+				setItems(data.items);
+				console.log("GET success:", data.items);
+				onLoadCompleted && onLoadCompleted();
+			})
+			.catch((error) => {
+				console.error("GET error:", error);
+			});
+	}, [onLoadCompleted]);
 
+	useEffect(() => {
+		if (reload) {
+			fetchItems();
+		}
+	}, [reload, fetchItems]);
 
-  useEffect(() => {
-    if (reload) {
-      fetchItems();
-    }
-  }, [reload, fetchItems]);
-
-  return (
-    <div className='ItemListGrid'>
-      {items.map((item) => {
-        return (
-          <div key={item.id} className='ItemList'>
-            {item.isImage ?
-              <div className="ItemImage" >
-                <Link to={`/items/${item.id}`}>
-                  <img src={server + "/image/" + item.image_filename} height="140" alt="No pic" />
-                </Link>
-              </div> :
-              <div className="ItemImage">
-                <Link to={`/items/${item.id}`}>
-                  <video muted height="140">
-                    <source src={server + "/image/" + item.image_filename} type="video/mp4" />
-                  </video>
-                </Link>
-              </div>}
-            <div className="ItemDescriptions">
-              <span className="ItemName">{item.name}</span>
-              <span className="ItemCategory">{item.category}</span>
-            </div>
-          </div>
-        )
-      })}
-    </div >
-  )
+	return (
+		<div className="ItemListGrid">
+			{items.map((item) => {
+				console.log(item);
+				return (
+					<div key={item.id} className="ItemList">
+						{item.isImage ? (
+							<div className="ItemImage">
+								<Link to={`/items/${item.id}`}>
+									<img
+										src={server + "/image/" + item.image_filename}
+										height="170"
+										alt="No pic"
+									/>
+								</Link>
+							</div>
+						) : (
+							<div className="ItemImage">
+								<Link to={`/items/${item.id}`}>
+									<video muted height="170">
+										<source
+											src={server + "/image/" + item.image_filename}
+											type="video/mp4"
+										/>
+									</video>
+								</Link>
+							</div>
+						)}
+						<div className="ItemDescriptions">
+							<span className="ItemName">{item.name}</span>
+							<div className="ItemCategoryWrapper">
+								<span className="ItemCategory">{item.category}</span>
+							</div>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
